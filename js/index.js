71d6a2b2c9992bc0d3e4f5fea427832aba31bdbf
Leti <ydna.andy74@gmail.com>
2024-08-25 17:32:32 -0300

    login

--- js/index.js
+++ js/index.js
@@ -17,12 +17,11 @@ document.addEventListener("DOMContentLoaded", function(){
         window.location = "products.html"
     });   
 })
+});
 // Simula un estado de autenticación
 let isAuthenticated = false; // Cambia esto a true cuando el usuario inicie sesión
 
 // Verifica si el usuario está autenticado
 if (!isAuthenticated) {
     // Si no está autenticado, redirige a la página de login
-    window.location.href = "login.html"
-};
-});
+    window.location.href = "login.html"}
