d83d0f8a6d5338e9c5c6729f75f92d5d75334382
jennSee <sivis3.js@gmail.com>
2024-08-30 16:13:48 -0300

    responsive

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(){
+    let usuario = localStorage.getItem("user");//recupero el usuario loggeado, si hay.
+    if (usuario==null){ //si no hay nadie loggeado
+        location.href="login.html";//lo mando a login.html
+    }
     document.getElementById("autos").addEventListener("click", function() {
         localStorage.setItem("catID", 101);
         window.location = "products.html"
--- js/login.js
+++ js/login.js
@@ -24,6 +24,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Validaciones
     function validarFormulario(event) {
+        event.preventDefault();
         let valido = true;
 
         
@@ -45,10 +46,16 @@ document.addEventListener("DOMContentLoaded", function() {
             ocultarError(contraseñaError, contraseñaErrorContenedor);
         }
 
-        if (!valido) {
-            event.preventDefault(); 
+        if (valido) {
+            localStorage.setItem("user", nombreInput.value.trim())
+            // Redirigir 
+            window.location = 'index.html'; 
         }
     }
 
     formulario.addEventListener('submit', validarFormulario);
 });
+
+
+
+
